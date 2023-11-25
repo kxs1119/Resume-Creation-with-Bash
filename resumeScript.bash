d2eb0056863f84557a05e17ea9664ca3437a5453
Kenny Smith <122045137+lilken621@users.noreply.github.com>
2023-11-24 22:03:56 -0500

    fix bugs after testing. added a Linkedin field

--- resumeScript.bash
+++ resumeScript.bash
@@ -3,8 +3,6 @@
 # Getting the information from the user 
 echo " Hello! Welcome to Resume Creation!"
 
-
-
 # Creating an intro 
 echo "What is your name?" 
 read name
@@ -15,6 +13,14 @@ read emailAddress
 echo "What is your phone number?"
 read phoneNumber
 
+echo "What is your LinkedIn?"
+read linkedin
+
+# Combine all sections 
+header="\033[1m$name\033[0m\n\n"    # ? Need to make name bold and centered
+header+="Contact Information:\n\n"
+header+="Email Address: $emailAddress\n|Phone: $phoneNumber\n|LinkedIn: $linkedin"
+
 size=3
 skillSize=6
 
@@ -22,7 +28,7 @@ skillSize=6
 echo "What school did you attend?"
 read schoolAttended    # school variable
 
-echo "List a skill: "
+echo "List skills you've learned from school: "
 # Creating the bullet points with no more than 3 bullet points 
 for ((i=0; i<$size;i++))
 do
@@ -39,65 +45,69 @@ echo "List bullet point indicating your experience: "
 for ((i=0; i<$size;i++))
 do
     
-    read experienceBulletin
-
+    read experienceBulletin\n
+    
 done
 
 # Creating Skills Section
-echo "List bullet point indicating your skillset: "
+echo "List bullet point indicating your skillset[type 'q' to quit]: "
 #? Need to add a test case if the user just presses a blank space
 for ((i=0; i<=$skillSize;i++))
 do
-   read skills
-
+   read skills\n
 done
 
 # Create a Development [volunteer work] section
 echo "Where have you volunteered"
-read volunteerWork
-
+read volunteerLocation
 echo "List bullet point indicating your volunteer work:  "
 for ((i=0; i<=$size;i++))
 do
-   read volunteerWork
+   read volunteerWork\n
 
 done
-
-# Combine all sections 
-header="$name\n\n"
-header+="Contact Information:\nEmail: $emailAddress\nPhone: $phoneNumber\n\n"
-header+="Education:$schoolAttended\n"
 
 resume="$header"
 
 # Adding eduction 
-resume+="Education:\n$schoolAttended\n"
+resume+="Education:\n\n"
+resume+="$schoolAttended\n\n"
 for ((i=0; i<$size;i++))
 do
-    resume+="* $schoolBulletPoint"
+    resume+="* $schoolBulletPoint\n\n"
 done
 
 # Adding work History
-resume+="Work Experience:\n$experience\n"
+resume+="Work Experience:\n"
+resume+="$experience\n"
+
 for ((i=0; i<$size;i++))
 do
-    resume+="* $experienceBulletin\n"
+    resume+="$experienceBulletin\n\n"
 done
 # Adding skills
-resume+="Skills:\n"
+resume+="Skills:\n\n"
 for ((i=0; i<$size;i++))
 do
-    resume+="* $skills\n"
+    resume+="* $skills\n\n"
+
 done
 # Adding volunteer work 
-resume+="Volunteer Work:\n"
+resume+="Professional Development:\n\n"
+resume+="$volunteerLocation\n"
 for ((i=0; i<$size;i++))
 do
-    resume+="* $volunteerWork\n"
+    resume+="* $volunteerWork\n\n"
+
 done
+
+# Adding a section to ask the user what job they are applying for. 
+# This will save as the pdf title
+echo "What job are you applying for?"
+read nameofJob
 
 # Print/Save document
 echo -e "$resume" | pandoc -o /home/rocky/cpsc207/resumeBuilder
 # Convert the resume to PDF and export 
-echo -e "$resume" | pandoc -o "$name.pdf"
+echo -e "$resume" | pandoc -o "$nameofJob.pdf"
 
